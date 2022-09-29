ac9f00874738907546562cd48617c456f7e49981
gobu-github <hgobulin.ds@gmail.com>
2022-09-29 10:46:34 -0300

    Tab fixes

--- TP2_Tkinter/exercise3.py
+++ TP2_Tkinter/exercise3.py
@@ -4,80 +4,80 @@ from tkinter import ttk
 
 # Create new instance - class Tk()
 root = Tk()
-root.iconbitmap('logo.ico')
-root.title('Customized Travel Solutions')
-root.geometry("1400x700")
-root.resizable(0, 0)
-root.config(bg = '#2B3A4A')
+root.iconbitmap ('logo.ico')
+root.title ('Customized Travel Solutions')
+root.geometry ("1400x700")
+root.resizable (0, 0)
+root.config (bg = '#2B3A4A')
 
 # Block 1 background photoImage
-block1 = Frame(root, width=1305, height=655)
-bgImage=PhotoImage(file="bgImage01.png")
-labelImage=Label(root, image=bgImage)
-labelImage.place(x=50, y=20)
+block1 = Frame (root, width = 1305, height = 655)
+bgImage = PhotoImage (file = "bgImage01.png")
+labelImage = Label (root, image = bgImage)
+labelImage.place (x = 50, y = 20)
 
 # Block Background 3 (labelFrame)
-block2 = Frame(root, width=575, height=320)
-block2.config(bg='#2B3A4A')
-block2.place(x=770, y=30)
+block2 = Frame (root, width = 575, height = 320)
+block2.config (bg = '#2B3A4A')
+block2.place (x = 770, y = 30)
 
 # Block 3 labelFrame CTS - Flights
-block3 = LabelFrame(root, text="CTS - Flights", width=520, height=300, fg="white", font="Arial 12")
-block3.config(bg="#2B3A4A")
+block3 = LabelFrame (root, text = "CTS - Flights", width = 520, height = 300, fg = "white", font = "Arial 12")
+block3.config (bg = "#2B3A4A")
 block3.pack()
-block3.place(x=780, y=35)
+block3.place (x = 780, y = 35)
 
 # Row 0 check buttons
-round_trip = Checkbutton(block3, text="Round trip", width=12, bg="#A1B8CF")
-round_trip.grid(row = 0, column = 0, padx=7,pady=15, sticky="w")
+round_trip = Checkbutton(block3, text = "Round trip", width = 12, bg = "#A1B8CF")
+round_trip.grid (row = 0, column = 0, padx = 7,pady = 15, sticky = "w")
 
-one_way = Checkbutton(block3, text="One Way", width=16, bg="#A1B8CF")
-one_way.grid(row = 0, column = 1, padx=0,pady=5, sticky="w")
+one_way = Checkbutton (block3, text = "One Way", width = 16, bg = "#A1B8CF")
+one_way.grid (row = 0, column = 1, padx = 0,pady = 5, sticky = "w")
 
-multi = Checkbutton(block3, text="Multi Destination", width=35, bg="#A1B8CF")
-multi.grid(row = 0, column = 2, padx=5,pady=5, sticky="w", columnspan=2)
+multi = Checkbutton (block3, text = "Multi Destination", width = 35, bg = "#A1B8CF")
+multi.grid (row = 0, column = 2, padx = 5,pady = 5, sticky = "w", columnspan = 2)
 
 # Row 1 boarding label
-boarding_label = Label(block3, text="- BOARDING -")
-boarding_label.grid(row=1, column=0, padx=5, pady=20, sticky="w")
-boarding_label.config(width = '15', bg = '#2B3A4A', fg="white", font="Arial 8 bold")
+boarding_label = Label (block3, text = "- BOARDING -")
+boarding_label.grid (row = 1, column = 0, padx = 5, pady = 20, sticky = "w")
+boarding_label.config (width = '15', bg = '#2B3A4A', fg = "white", font = "Arial 8 bold")
 
 # Row 1 boarding entry
-boarding_entry = Entry(block3, width = '15', bg="#2B3A4A", fg="white", font="Arial 12")
-boarding_entry.grid(row=1, column=1, sticky="w")
+boarding_entry = Entry (block3, width = '15', bg = "#2B3A4A", fg = "white", font = "Arial 12")
+boarding_entry.grid (row = 1, column = 1, sticky = "w")
 
 # Row 1 destination label 
-destination_label = Label(block3, text="- DESTINATION -")
-destination_label.grid(row=1, column=2, padx=5, pady=20, sticky="w")
-destination_label.config(width = '15', bg = '#2B3A4A', fg="white", font="Arial 8 bold")
+destination_label = Label (block3, text = "- DESTINATION -")
+destination_label.grid (row = 1, column = 2, padx = 5, pady = 20, sticky = "w")
+destination_label.config (width = '15', bg = '#2B3A4A', fg = "white", font = "Arial 8 bold")
 
 # Row 1 destination entry 
-destination_entry = Entry(block3, width = '17', bg="#2B3A4A", fg="white", font="Arial 12")
-destination_entry.grid(row=1, column=3, sticky="w")
+destination_entry = Entry (block3, width = '17', bg = "#2B3A4A", fg = "white", font = "Arial 12")
+destination_entry.grid (row = 1, column = 3, sticky = "w")
 
 # Row 2 passengers label
-passengers = Label(block3, text="Passengers", bg="#15202B")
-passengers.grid(row=2, column=0, padx=5, pady=5, sticky="e")
-passengers.config(width = '16', bg = '#15202B', fg="white")
+passengers = Label (block3, text = "Passengers", bg = "#15202B")
+passengers.grid (row = 2, column = 0, padx = 5, pady = 5, sticky="e")
+passengers.config (width = '16', bg = '#15202B', fg = "white")
 
 # Row 2 passengers combobox 
-passengers_box = ttk.Combobox(block3)
-passengers_box.grid(row = 2, column = 1, sticky="w")
-passengers_box['values']=('1', '2', '3', '4', '5','6','7','8','9','10')
-passengers_box.set('1')
-passengers_box.config(width=5)
+passengers_box = ttk.Combobox (block3)
+passengers_box.grid (row = 2, column = 1, sticky = "w")
+passengers_box ['values'] = ('1', '2', '3', '4', '5','6','7','8','9','10')
+passengers_box.set ('1')
+passengers_box.config (width = 5)
 
 # Row 3 div (separator)
-div_separator = Frame(block3)
-div_separator.grid(row = 3, column = 0, columnspan = 2)
-div_separator.config(height='75', bg = '#2B3A4A')
+div_separator = Frame (block3)
+div_separator.grid (row = 3, column = 0, columnspan = 2)
+div_separator.config (height = '75', bg = '#2B3A4A')
 
 # Row 4 buttons exit and reserve
-exit_btn = Button(block3, text = 'Exit', font = 'Arial 12 bold', bg = '#15202B', fg = 'white', width=15)
-exit_btn.grid(row = 4, column = 0, pady=5, columnspan = 2)
+exit_btn = Button (block3, text = 'Exit', font = 'Arial 12 bold', bg = '#15202B', fg = 'white', width = 15)
+exit_btn.grid (row = 4, column = 0, pady = 5, columnspan = 2)
 
-reserve_btn = Button(block3, text = 'Reserve', font = 'Arial 12 bold', bg = '#15202B', fg = 'white', width=15)
-reserve_btn.grid(row = 4, column = 2, pady=15, columnspan = 2)
+reserve_btn = Button (block3, text = 'Reserve', font = 'Arial 12 bold', bg = '#15202B', fg = 'white', width = 15)
+reserve_btn.grid (row = 4, column = 2, pady = 15, columnspan = 2)
 
 
 root.mainloop()
--- TP2_Tkinter/exercise_1_2.py
+++ TP2_Tkinter/exercise_1_2.py
@@ -4,19 +4,19 @@ from tkinter import ttk
 from msilib.schema import ComboBox
 
 # function to validate 8 characters on document field
-def validate_docNumber(text1, newText1):
-    if len(newText1) > 8:
-        messagebox.showinfo("", "8 characters max")
+def validate_docNumber (text1, newText1):
+    if len (newText1) > 8:
+        messagebox.showinfo ("", "8 characters max")
         return False
     return text1.isdecimal()
 
 # function to validate only 4 characters and year less than 2022 in the year field
-def validate_year(text2, newText2):
-    if len(newText2) > 4:
-        messagebox.showinfo("", "4 characters max")
+def validate_year (text2, newText2):
+    if len (newText2) > 4:
+        messagebox.showinfo ("", "4 characters max")
         return False
     elif (newText2) > "2022":
-        messagebox.showinfo("", "Please, enter a valid year")
+        messagebox.showinfo ("", "Please, enter a valid year")
         return False
     return text2.isdecimal()
 
@@ -33,170 +33,169 @@ def login():
         screen.iconify()
         global screen1
         
-        screen1 = Toplevel(screen)
-        screen1.iconbitmap( 'logo.ico' )
-        screen1.title( 'Form - Python' )
-        screen1.resizable( 0, 0 )
-        screen1.config( background = '#2B3A4A' )
+        screen1 = Toplevel (screen)
+        screen1.iconbitmap ('logo.ico')
+        screen1.title ('Form - Python')
+        screen1.resizable (0, 0)
+        screen1.config (background = '#2B3A4A')
 
         # block 7 header
-        block7 = Frame(screen1)
-        block7.grid(row = 0, column = 0, columnspan=2)
-        block7.config(width ='400', height='50', bg = '#2B3A4A')
-        header_label = Label(screen1, text = 'Welcome Admin', fg = 'white', font = 'Cambria 15 bold', background = '#2B3A4A' )
-        header_label.grid( row = 0, column = 0, columnspan=2 )
+        block7 = Frame (screen1)
+        block7.grid (row = 0, column = 0, columnspan = 2)
+        block7.config (width = '400', height ='50', bg = '#2B3A4A')
+        header_label = Label (screen1, text = 'Welcome Admin', fg = 'white', font = 'Cambria 15 bold', background = '#2B3A4A')
+        header_label.grid (row = 0, column = 0, columnspan=2)
 
         # block 8 logo
-        block8 = Frame(screen1)
-        block8.grid(row = 1, column = 0, columnspan=2)
-        block8.config(width ='400', height='60', bg = '#15202B')
+        block8 = Frame (screen1)
+        block8.grid (row = 1, column = 0, columnspan = 2)
+        block8.config (width = '400', height ='60', bg = '#15202B')
 
         # block 9 name label
-        block9 = Frame(screen1)
-        block9.grid(row = 2, column = 0)
-        block9.config(width ='150', height='50', bg = '#2B3A4A')
-        name_label = Label(screen1, text = 'Name', fg = 'white', font = 'Cambria 11', background = '#2B3A4A' )
-        name_label.grid( row = 2, column = 0, sticky="e", padx=10 )
+        block9 = Frame (screen1)
+        block9.grid (row = 2, column = 0)
+        block9.config (width ='150', height ='50', bg = '#2B3A4A')
+        name_label = Label (screen1, text = 'Name', fg = 'white', font = 'Cambria 11', background = '#2B3A4A')
+        name_label.grid (row = 2, column = 0, sticky = "e", padx = 10)
 
         # block 10 name entry 
-        block10 = Frame(screen1)
-        block10.grid( row = 2, column = 1 )
-        block10.config( width = '250', height = '50', bg = '#2B3A4A' )
+        block10 = Frame (screen1)
+        block10.grid ( row = 2, column = 1 )
+        block10.config ( width = '250', height = '50', bg = '#2B3A4A')
         name = StringVar()
-        name_entry = Entry(screen1, textvariable = name, width = '30' )
-        name_entry.grid( row = 2, column = 1, sticky="w" )
+        name_entry = Entry(screen1, textvariable = name, width = '30')
+        name_entry.grid ( row = 2, column = 1, sticky="w")
 
         # block 11 surname label
-        block11 = Frame(screen1)
-        block11.grid(row = 3, column = 0)
-        block11.config(width ='150', height='50', bg = '#2B3A4A')
-        surname = Label(screen1, text = 'Surname', fg = 'white', font = 'Cambria 11', background = '#2B3A4A' )
-        surname.grid( row = 3, column = 0, sticky="e", padx=10)
+        block11 = Frame (screen1)
+        block11.grid (row = 3, column = 0)
+        block11.config (width ='150', height ='50', bg = '#2B3A4A')
+        surname = Label (screen1, text = 'Surname', fg = 'white', font = 'Cambria 11', background = '#2B3A4A')
+        surname.grid (row = 3, column = 0, sticky = "e", padx = 10)
 
         # block 12 surname entry 
-        block12 = Frame(screen1)
-        block12.grid( row = 3, column = 1 )
-        block12.config( width = '250', height = '50', bg = '#2B3A4A' )
+        block12 = Frame (screen1)
+        block12.grid (row = 3, column = 1)
+        block12.config ( width = '250', height = '50', bg = '#2B3A4A')
         surname = StringVar()
-        surname_entry = Entry(screen1, textvariable = surname, width = '30' )
-        surname_entry.grid( row = 3, column = 1, sticky="w")
+        surname_entry = Entry(screen1, textvariable = surname, width = '30')
+        surname_entry.grid (row = 3, column = 1, sticky = "w")
 
         # Docs type
-        docsType_box = ttk.Combobox(screen1)
-        docsType_box.grid(row = 4, column = 0, sticky="e", padx=10)
-        docsType_box['values']=('DNI', 'LE', 'LC', 'CI', 'PAS')
-        docsType_box.set('DNI')
-        docsType_box.config(width=6)
+        docsType_box = ttk.Combobox (screen1)
+        docsType_box.grid (row = 4, column = 0, sticky="e", padx = 10)
+        docsType_box ['values'] = ('DNI', 'LE', 'LC', 'CI', 'PAS')
+        docsType_box.set ('DNI')
+        docsType_box.config (width = 6)
 
         # block 13 doc number entry 
-        block13 = Frame(screen1)
-        block13.grid( row = 4, column = 1 )
-        block13.config( width = '250', height = '50', bg = '#2B3A4A' )
-        docNumber = ttk.Entry(screen1, validate="key",width = '30', validatecommand=(screen1.register(validate_docNumber), "%S", "%P")) # "%S", "%P" are the arguments)
-        docNumber.grid( row = 4, column = 1, sticky="w", pady=10 )
+        block13 = Frame (screen1)
+        block13.grid (row = 4, column = 1)
+        block13.config (width = '250', height = '50', bg = '#2B3A4A')
+        docNumber = ttk.Entry (screen1, validate = "key",width = '30', validatecommand = (screen1.register (validate_docNumber), "%S", "%P"))
+        docNumber.grid (row = 4, column = 1, sticky = "w", pady = 10 )
 
         # block 14 birthday label
-        block14 = Frame(screen1)
-        block14.grid(row = 5, column = 0)
-        block14.config(width ='150', height='50', bg = '#2B3A4A')
-        birthday = Label(screen1, text = 'Birthday', fg = 'white', font = 'Cambria 11', background = '#2B3A4A' )
-        birthday.grid( row = 5, column = 0, sticky="e", padx=10)
+        block14 = Frame (screen1)
+        block14.grid (row = 5, column = 0)
+        block14.config (width ='150', height = '50', bg = '#2B3A4A')
+        birthday = Label (screen1, text = 'Birthday', fg = 'white', font = 'Cambria 11', background = '#2B3A4A')
+        birthday.grid (row = 5, column = 0, sticky="e", padx = 10)
 
         # block day box
-        day_box = ttk.Combobox(screen1)
-        day_box.grid(row = 5, column = 1, sticky="w")
-        day_box['values']=('1', '2', '3', '4', '5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31')
-        day_box.set('Day')
-        day_box.config(width=5)
+        day_box = ttk.Combobox (screen1)
+        day_box.grid (row = 5, column = 1, sticky = "w")
+        day_box['values'] = ('1', '2', '3', '4', '5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31')
+        day_box.set ('Day')
+        day_box.config (width = 5)
 
         # block month box
-        month_box = ttk.Combobox(screen1)
-        month_box.place(x=207, y=274)
-        month_box['values']=('January', 'February', 'March', 'April', 'May', "June", "July", "August", "September", "Octuber", "November", "December")
-        month_box.set('Month')
-        month_box.config(width=11)
+        month_box = ttk.Combobox (screen1)
+        month_box.place (x = 207, y = 274)
+        month_box ['values'] = ('January', 'February', 'March', 'April', 'May', "June", "July", "August", "September", "Octuber", "November", "December")
+        month_box.set ('Month')
+        month_box.config (width = 11)
 
-        # block year entry 
-        year = StringVar(screen1)
-        year_entry = ttk.Entry(screen1, validate="key",width = '5',  validatecommand=(screen1.register(validate_year), "%S", "%P"))
-        year_entry.place( x=300, y=274 )
+        # block year entry
+        year_entry = ttk.Entry (screen1, validate ="key",width = '5',  validatecommand = (screen1.register(validate_year), "%S", "%P"))
+        year_entry.place (x = 300, y = 274)
 
         # block 15 footer
-        block15 = Frame(screen1)
-        block15.grid(row = 6, column = 0, columnspan=2)
-        block15.config(width ='400', height='100', bg = '#2B3A4A')
+        block15 = Frame (screen1)
+        block15.grid (row = 6, column = 0, columnspan = 2)
+        block15.config (width ='400', height ='100', bg = '#2B3A4A')
 
     elif (username == "" or password == ""):
-        messagebox.showinfo("", "Fields Blank Not Allowed")
+        messagebox.showinfo ("", "Fields Blank Not Allowed")
 
     else:
-        messagebox.showinfo("", "Incorrect Username or Password")
+        messagebox.showinfo ("", "Incorrect Username or Password")
 
 # main screen
 def main_screen():
     global screen
-    screen= Tk()
+    screen = Tk()
 
-    screen.iconbitmap('logo.ico')
-    screen.title('Form - Python')
-    screen.resizable(0, 0)
-    screen.config(background = '#2B3A4A')
+    screen.iconbitmap ('logo.ico')
+    screen.title ('Form - Python')
+    screen.resizable (0, 0)
+    screen.config (background = '#2B3A4A')
 
     # block 0 header
     block0 = Frame()
-    block0.grid(row = 0, column = 0, columnspan = 2)
-    block0.config(width ='400', height='50', bg = '#2B3A4A')
-    header_label = Label(screen, text = 'Tkinter - Access', fg = 'white', font = 'Cambria 15 bold', background = '#2B3A4A')
-    header_label.grid(row = 0, column = 0, columnspan=2)
+    block0.grid (row = 0, column = 0, columnspan = 2)
+    block0.config (width ='400', height = '50', bg = '#2B3A4A')
+    header_label = Label (screen, text = 'Tkinter - Access', fg = 'white', font = 'Cambria 15 bold', background = '#2B3A4A')
+    header_label.grid (row = 0, column = 0, columnspan = 2)
     
     # block 1 logo
     block1 = Frame()
-    block1.grid(row = 1, column = 0, columnspan = 2)
-    block1.config(width = '400', height = '60', bg = '#15202B')
-    python_image=PhotoImage(file = 'python.png')
-    label=Label(screen, image = python_image, bg = '#15202B')
-    label.grid(row = 1, column = 0, columnspan = 2)
+    block1.grid (row = 1, column = 0, columnspan = 2)
+    block1.config (width = '400', height = '60', bg = '#15202B')
+    python_image = PhotoImage (file = 'python.png')
+    label = Label (screen, image = python_image, bg = '#15202B')
+    label.grid (row = 1, column = 0, columnspan = 2)
 
     # block 2 user label
     block2 = Frame()
-    block2.grid(row = 2, column = 0)
-    block2.config(width ='150', height='50', bg = '#2B3A4A')
-    user_label = Label(screen, text = 'Name', fg = 'white', font = 'Cambria 11', background = '#2B3A4A')
-    user_label.grid(row = 2, column = 0, sticky = "e", padx = 10)
+    block2.grid (row = 2, column = 0)
+    block2.config (width ='150', height = '50', bg = '#2B3A4A')
+    user_label = Label (screen, text = 'Name', fg = 'white', font = 'Cambria 11', background = '#2B3A4A')
+    user_label.grid (row = 2, column = 0, sticky = "e", padx = 10)
 
     # block 3 name entry 
     block3 = Frame()
-    block3.grid(row = 2, column = 1)
-    block3.config(width = '250', height = '50', bg = '#2B3A4A')
+    block3.grid (row = 2, column = 1)
+    block3.config (width = '250', height = '50', bg = '#2B3A4A')
     user = StringVar()
     global user_entry
-    user_entry = Entry(screen, textvariable = user, width = '30')
-    user_entry.grid(row = 2, column = 1, sticky = "w")
+    user_entry = Entry (screen, textvariable = user, width = '30')
+    user_entry.grid (row = 2, column = 1, sticky = "w")
 
     # block 4 password label
     block4 = Frame()
-    block4.grid(row = 3, column = 0)
-    block4.config(width ='150', height='50', bg = '#2B3A4A')
-    password_label = Label(screen, text = 'Password', fg = 'white', font = 'Cambria 11', background = '#2B3A4A')
-    password_label.grid(row = 3, column = 0, sticky = "e", padx = 10)
+    block4.grid (row = 3, column = 0)
+    block4.config (width ='150', height = '50', bg = '#2B3A4A')
+    password_label = Label (screen, text = 'Password', fg = 'white', font = 'Cambria 11', background = '#2B3A4A')
+    password_label.grid (row = 3, column = 0, sticky = "e", padx = 10)
 
     # block 5 password entry 
     block5 = Frame()
-    block5.grid(row = 3, column = 1)
-    block5.config(width = '250', height = '50', bg = '#2B3A4A')
+    block5.grid (row = 3, column = 1)
+    block5.config (width = '250', height = '50', bg = '#2B3A4A')
     password = StringVar()
     global password_entry
-    password_entry = Entry(screen, textvariable = password, width = '30', show = "*")
-    password_entry.grid(row = 3, column = 1, sticky = "w")
+    password_entry = Entry (screen, textvariable = password, width = '30', show = "*")
+    password_entry.grid (row = 3, column = 1, sticky = "w")
 
     # block 6 separator
     block6 = Frame()
-    block6.grid(row = 4, column = 0, columnspan = 2)
-    block6.config(width = '400', height = '100', bg = '#2B3A4A')
+    block6.grid (row = 4, column = 0, columnspan = 2)
+    block6.config (width = '400', height = '100', bg = '#2B3A4A')
 
     # block 7 button
-    btn = Button(text = 'Login', width="20", command = login, font = 'Cambria 12', bg = '#15202B', fg = 'white')
-    btn.grid(row = 4, column = 0, columnspan = 2)
+    btn = Button (text = 'Login', width = "20", command = login, font = 'Cambria 12', bg = '#15202B', fg = 'white')
+    btn.grid (row = 4, column = 0, columnspan = 2)
     
 
     screen.mainloop()
