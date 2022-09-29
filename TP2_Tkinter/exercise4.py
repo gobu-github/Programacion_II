9cfa742374576e663c2d8b2c8bc35114a6273b64
gobu-github <hgobulin.ds@gmail.com>
2022-09-28 21:54:57 -0300

    Exercise 4 approved

--- TP2_Tkinter/exercise4.py
+++ TP2_Tkinter/exercise4.py
@@ -1,7 +1,11 @@
 # 4 Utilizando los widgets Menu y Notebook diseñar una pantalla de Menú/Solapas de la temática que desees.
 
 #import tkinter 
+from cProfile import label
 from tkinter import*
+from tkinter import ttk
+from tkinter.tix import NoteBook
+from turtle import bgcolor, color
 
 # Create new instance Tk()
 root=Tk()
@@ -11,46 +15,107 @@ root.resizable (0, 0)
 
 # Create menu bar
 menu_bar = Menu (root)
-root.config (menu=menu_bar, width=500, height=300, bg='#2B3A4A')
+root.config (menu=menu_bar, width=800, height=500, bg='#2B3A4A')
 
-# Archive menu
-archiveMenu=Menu (menu_bar, tearoff=0) 
-menu_bar.add_cascade (label="Archive", menu=archiveMenu) 
-archiveMenu.add_command (label="New") #Item
-archiveMenu.add_command (label="Save") #Item
+# File menu
+fileMenu=Menu (menu_bar, tearoff=0) 
+menu_bar.add_cascade (label="File", menu=fileMenu) 
+fileMenu.add_command (label="New") #Item
+fileMenu.add_command (label="Open") #Item
+
+# Open Recent submenu
+openRecentMenu=Menu (menu_bar, tearoff=0)
+fileMenu.add_cascade (label="Open Recent", menu=openRecentMenu)
+openRecentMenu.add_command (label="My file 1") #Item
+openRecentMenu.add_command (label="My file 2") #Item
+openRecentMenu.add_command (label="My file 3") #Item
+
+fileMenu.add_command (label="Save") #Item
 
 # Save as submenu
 saveAsMenu=Menu (menu_bar, tearoff=0) 
-archiveMenu.add_cascade (label="Save as", menu=saveAsMenu) 
+fileMenu.add_cascade (label="Save as", menu=saveAsMenu) 
 saveAsMenu.add_command (label="PDF") #Item
 saveAsMenu.add_command (label="PNG") #Item
 saveAsMenu.add_command (label="JPG") #Item
 
 # separator
-archiveMenu.add_separator () 
+fileMenu.add_separator () 
 
-archiveMenu.add_command (label="Close") #Item
-archiveMenu.add_command (label="Exit") #Item
+fileMenu.add_command (label="Close") #Item
+fileMenu.add_command (label="Exit") #Item
 
-# Edition menu
-archiveEdition=Menu (menu_bar, tearoff=0)
-menu_bar.add_cascade  (label="Edition", menu=archiveEdition) 
-archiveEdition.add_command (label="Copy") #Item
-archiveEdition.add_command  (label="Cut") #Item
-archiveEdition.add_command (label="Paste") #Item
+# Edit menu
+editMenu=Menu (menu_bar, tearoff=0)
+menu_bar.add_cascade  (label="Edit", menu=editMenu) 
+editMenu.add_command (label="Copy") #Item
+editMenu.add_command  (label="Cut") #Item
+editMenu.add_command (label="Paste") #Item
 
-# Tools menu
-archiveTools=Menu (menu_bar, tearoff=0)
-menu_bar.add_cascade (label="Tools", menu=archiveTools) 
-archiveTools.add_command (label="one") #Item
-archiveTools.add_command (label="two") #Item
-archiveTools.add_command (label="three") #Item
-archiveTools.add_command (label="four") #Item
+# Terminal menu
+terminalMenu=Menu (menu_bar, tearoff=0)
+menu_bar.add_cascade (label="Terminal", menu=terminalMenu) 
+terminalMenu.add_command (label="New Terminal") #Item
+terminalMenu.add_command (label="Split Terminal") #Item
+
+# Separator
+terminalMenu.add_separator () 
+
+terminalMenu.add_command (label="Run Task") #Item
+terminalMenu.add_command (label="Run Build Task") #Item
+
+# Run menu
+runMenu=Menu (menu_bar, tearoff=0)
+menu_bar.add_cascade (label="Run", menu=runMenu) 
+runMenu.add_command (label="Start Debugging") #Item
+runMenu.add_command (label="Start Without Debugging") #Item
 
 # Help menu
-archiveHelp=Menu (menu_bar, tearoff=0)
-menu_bar.add_cascade (label="Help", menu=archiveHelp) 
-archiveHelp.add_command (label="License") #Item
-archiveHelp.add_command (label="About") #Item
+helpMenu=Menu (menu_bar, tearoff=0)
+menu_bar.add_cascade (label="Help", menu=helpMenu) 
+helpMenu.add_command (label="License") #Item
+helpMenu.add_command (label="About Python") #Item
+
+# block 15 footer
+block15 = Frame(root)
+block15.config(width ='800', height='2', bg = '#EB7A5D')
+block15.pack()
+block15 = Frame(root)
+block15.config(width ='800', height='2', bg = '#3A424F')
+block15.pack()
+block15 = Frame(root)
+block15.config(width ='800', height='2', bg = '#EB7A5D')
+block15.pack()
+
+
+#Incluimos panel para pastañas root = ttk.Notebook (ventanal)
+archive=ttk.Notebook(root)
+archive.config(width=800, height=500)
+archive.pack () 
+#Creamos las pestañas
+file_1 = Frame (archive)
+file_2 = Frame (archive)
+file_3 = Frame (archive)
+file_4 = Frame (archive)
+#Agregamos pestañas creadas
+archive.add (file_1, text="Label_1" ) 
+archive.add (file_2, text="Label_2")
+archive.add (file_3, text="Label_3")
+archive.add (file_4, text="Label_4")
+
+file_1.config(bg='#3A424F')
+Label(file_1, text="Label-1", font="Calibri 25", fg="white", bg='#3A424F').place(x=350, y=200)
+file_2.config(bg='#72819C')
+Label(file_2, text="Label-2", font="Calibri 25", fg="white", bg='#72819C').place(x=350, y=200)
+file_3.config(bg='#323D4F')
+Label(file_3, text="Label-3", font="Calibri 25", fg="white", bg='#323D4F').place(x=350, y=200)
+file_4.config(bg='#2D4369')
+Label(file_4, text="Label-4", font="Calibri 25", fg="white", bg='#2D4369').place(x=350, y=200)
+
+
+
+
+
+
 
 root.mainloop()
